f60a8393590ab430f70a84dbcd0b727d44926e2d
Diana Vodopivec <dianavodopivec@MacBook-Air-de-Matias.local>
2024-02-12 13:05:57 -0300

    fix: fixing earthTime js

--- assets/scripts/clock.js
+++ assets/scripts/clock.js
@@ -1,5 +1,5 @@
 const $containerClock = document.getElementById("clock");
-const $earth = document.getElementById("earth")
+const $earth = document.getElementById("earth");
 let runningTime 
 
 const clockFuncionality = () => {
@@ -7,17 +7,19 @@ const clockFuncionality = () => {
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    $containerClock.innerHTML = `${hours}:${minutes}:${seconds}` 
+    $containerClock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
+
 
 const earthChanges = () => {
     const date = new Date();
     const hours = date.getHours();
-    if(hours >= 20) {
+    if(hours >= 20 || hours <= 5) {
+        console.log("noche")
         $earth.style.backgroundImage = "url(/assets/images/earth-planet-night.jpg)";
         $earth.style.boxShadow = "box-shadow: inset 15px 0px 20px -2px rgba(100, 99, 99, 0.577), inset -70px 0px 50px 0px #000000, -5px 0px 30px -4px rgb(12, 32, 40);"
-    } 
-    if(hours <= 1) {
+    } else {
+        console.log("dia")
         $earth.style.backgroundImage = "url(/assets/images/earth-planet.jpeg)";
         $earth.style.boxShadow = "none"
     }
