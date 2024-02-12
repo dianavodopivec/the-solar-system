db6da3c35afffb656cf8acde6801b7e8c68290fb
Diana Vodopivec <dianavodopivec@MacBook-Air-de-Matias.local>
2024-02-11 21:45:28 -0300

    removing gsap

--- assets/scripts/clock.js
+++ assets/scripts/clock.js
@@ -13,10 +13,11 @@ const clockFuncionality = () => {
 const earthChanges = () => {
     const date = new Date();
     const hours = date.getHours();
-    if(hours >= 20 || hours < 21) {
+    if(hours >= 20 || hours < 22) {
         $earth.style.backgroundImage = "url(/assets/images/earth-planet-night.jpg)";
         $earth.style.boxShadow = "box-shadow: inset 15px 0px 20px -2px rgba(100, 99, 99, 0.577), inset -70px 0px 50px 0px #000000, -5px 0px 30px -4px rgb(12, 32, 40);"
     } else {
+        $earth.classList.remove("earth-night");
         $earth.style.backgroundImage = "url(/assets/images/earth-planet.jpeg)";
         $earth.style.boxShadow = "none"
     }
