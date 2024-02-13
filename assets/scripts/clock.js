a70788b6a411a02bfaad97ae1a51ad1ef8e579b3
Diana Vodopivec <dianavodopivec@MacBook-Air-de-Matias.local>
2024-02-13 13:41:37 -0300

    feat: exclusive file for earthchanges.js

--- assets/scripts/clock.js
+++ assets/scripts/clock.js
@@ -1,5 +1,4 @@
 const $containerClock = document.getElementById("clock");
-const $earth = document.getElementById("earth");
 let runningTime 
 
 const clockFuncionality = () => {
@@ -14,29 +13,15 @@ const renderClock = (container, hou, min, sec) => {
     container.innerHTML = `${hou}:${min}:${sec}`
 }
 
-const earthChanges = () => {
-    const date = new Date();
-    const hours = date.getHours();
-    if (hours >= 20 || hours <= 5) {
-        $earth.style.backgroundImage = "url(/assets/images/earth-planet-night.jpg)";
-        $earth.style.boxShadow = "box-shadow: inset 15px 0px 20px -2px rgba(100, 99, 99, 0.577), inset -70px 0px 50px 0px #000000, -5px 0px 30px -4px rgb(12, 32, 40);"
-    } else {
-        $earth.style.backgroundImage = "url(/assets/images/earth-planet.jpeg)";
-        $earth.style.boxShadow = "none"
-    }
-}
-
 renderClock($containerClock, clockFuncionality().hours,clockFuncionality().minutes,clockFuncionality().seconds);
 
 runningTime = setInterval(() => {
     clockFuncionality();
     renderClock($containerClock, clockFuncionality().hours,clockFuncionality().minutes,clockFuncionality().seconds);
-    earthChanges();
 }, 1000);
 
 
 export { 
-    clockFuncionality,
-    earthChanges
+    clockFuncionality
 };
 
--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 import { 
     clockFuncionality,
-    earthChanges
-} from "/assets/scripts/clock.js";
+} from "./assets/scripts/clock.js";
 
+import { earthChanges 
+} from "./assets/scripts/earthChanges.js";
+
+//To execute js code that interacts with the DOM
+document.addEventListener("DOMContentLoaded", e => {
+    clockFuncionality();
+    earthChanges();
+})
+
+//---------- GSAP TESTING ----------//
+
+// const $sun = document.getElementById("sun");
+// console.log($sun)
+
+// gsap.set($sun.children, { opacity: 1, y: 500, rotation: 0 }); 
+
+// gsap.to($sun.children, {
+//   opacity: 1,
+//   scrollTrigger: {
+//     trigger: ".container-planets",
+//     start: "top center",
+//     toggleActions: "restart pause pause pause",
+//     scrub: true,
+//   },
+//   y: 0,
+//   rotation: 0,
+//   duration: 4,
+// });
