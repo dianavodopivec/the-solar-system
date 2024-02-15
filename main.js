4da456aaa1c14b814c4d46238fe94e588e97c320
Diana Vodopivec <dianavodopivec@MacBook-Air-de-Matias.local>
2024-02-15 12:55:31 -0300

    testing

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import { clockFuncionality } from "./assets/scripts/clock.js";
-/*import { earthChanges } from "./assets/scripts/earthChanges.js";*/
+const $geolocationContainer = document.getElementById("geolocation-info");
 const $title = document.querySelector("h1");
 const $title2 = document.querySelector("h2");
+const $star = document.querySelector(".button-play");
+const $containerInfo = document.querySelector(".container-info");
+/*import { earthChanges 
+} from "./assets/scripts/earthChanges.js";*/
 
 //To execute js code that interacts with the DOM
 document.addEventListener("DOMContentLoaded", e => {
@@ -59,7 +63,6 @@ tl.to(".wrap-scroll", {
 gsap.ticker.lagSmoothing(0);
 
 gsap.to($title, {
-  opacity: 1,
   scrollTrigger: {
     trigger: ".container-planet",
     start: "top, bottom",
@@ -69,10 +72,10 @@ gsap.to($title, {
   },
   y: -80,
   scale: 0.95,
+  opacity: 0,
 });
 
 gsap.to($title2, {
-  opacity: 1,
   scrollTrigger: {
     trigger: ".container-planet",
     start: "top, bottom",
@@ -82,4 +85,50 @@ gsap.to($title2, {
   },
   y: -100,
   scale: 0.95,
+  opacity: 0,
 });
+
+gsap.to($star, {
+  scrollTrigger: {
+    trigger: ".container-planet",
+    start: "top, bottom",
+    end: "+=1200",
+    toggleActions: "restart pause reverse pause",
+    scrub: 0,
+  },
+  y: 200,
+  scale: 0.55,
+  opacity: 0,
+});
+
+const getGeolocation = async () => {
+  if (navigator.geolocation) {
+    try {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      const { latitude, longitude } = position.coords;
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+      );
+
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const data = await response.json();
+      const country = data.address.country;
+      const state = data.address.state;
+
+      console.log(data.address);
+      const formattedCountry = country.toUpperCase().slice(0, 3);
+      $geolocationContainer.innerText = `${formattedCountry} ${state}`;
+    } catch (error) {
+      console.error(error.message);
+    }
+  } else {
+    $geolocationContainer.innerText = "geolocation locked";
+  }
+};
+
+getGeolocation();
